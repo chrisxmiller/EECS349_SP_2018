18c5bf07c9e7ef718bfbb24add20c9897f8f71d4
Chris Miller <chrisxmiller@users.noreply.github.com>
2018-04-12 18:46:20 -0500

    updated runner file

--- 001_Homework/runner.py
+++ 001_Homework/runner.py
@@ -17,10 +17,6 @@ data = [dict(a=1, b=0, Class=2),
     	dict(a=3, b=0, Class=1),
     	dict(a=3, b=1, Class=3)]
 
-tel = {'jack': 4098, 'sape': 4139}
-
-a = dict(one=1, two=2, three=3)
-
 
 #ent = ID3.entropy(data)
 print data[1].keys()
