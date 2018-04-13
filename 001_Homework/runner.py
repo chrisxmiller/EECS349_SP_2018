4ffb11450834094419e3da698f128eb33673e1d3
Chris Miller <chrisxmiller@users.noreply.github.com>
2018-04-12 19:24:55 -0500

    num attributes, index of class complete

--- 001_Homework/runner.py
+++ 001_Homework/runner.py
@@ -10,13 +10,38 @@ import parse
 
 
 #Read in the data
-data = [dict(a=1, b=0, Class=2), 
+data2 = [dict(a=1, b=0, Class=2), 
 		dict(a=1, b=1, Class=1),
 	    dict(a=2, b=0, Class=2), 	
 	    dict(a=2, b=1, Class=3),
     	dict(a=3, b=0, Class=1),
     	dict(a=3, b=1, Class=3)]
 
+data =  [dict(a=1, b=0,   c='?', Class=1), 
+		 dict(a=1, b=3,   c=2,   Class=1),
+		 dict(a=2, b='?', c=1,   Class=2), 
+		 dict(a=2, b=1,   c=3,   Class=2),
+         dict(a=3, b=0,   c=1,   Class=3), 
+         dict(a=3, b=2,   c='?', Class=3)]
+
+
+#Num keys -1 (last is the class)
+num_attributes = len(data[1].keys())-1
+
+
+#Find index in the loop
+classidx = 0
+for i in range(0, num_attributes+1):
+	if(data[1].keys()[i] == "Class"):
+		classidx = i
+		break
+
+
+
+
+
+
+
 
 #ent = ID3.entropy(data)
-print data[1].keys()
+print classidx
