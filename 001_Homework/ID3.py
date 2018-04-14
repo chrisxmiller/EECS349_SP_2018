6022f18c3738b58c87bb840e2114da2b1d625b79
elutzen <eluzen@gmail.com>
2018-04-14 11:48:05 -0700

    The runner was used to test a bunch of functions to place in ID3. findBestAttribute-complete entropy-complete mode-complete a few other simple functions were added

--- 001_Homework/ID3.py
+++ 001_Homework/ID3.py
@@ -8,6 +8,74 @@ def ID3(examples, default):
   and the target class variable is a special attribute with the name "Class".
   Any missing attributes are denoted with a value of "?"
   '''
+  if len(examples) == 0:
+    return node(default)
+  elif sameClass(examples):
+    return node(mode(examples))
+  else:
+    best = bestAttribute(examples)
+    #Find different values in for attribute best
+    values = list(set(list([example[best] for example in examples])))
+    t = node() with root best
+    for value in values:
+      #split examples for the different values of best
+      newExamples = splitExamples = [example for example in examples if example[attribute] == value]
+      subtree = ID3(newExamples,mode(newExamples))
+      t = add branch subtree with label value to t
+  return t
+
+#Returns the mode classification from the examples
+def mode(examples):
+  #Get list of all classification for the examples
+  values = list([example['Class'] for example in examples])
+  #Get the list of possible classification
+  labels = list(set(values))
+  #Find number of instances of each label
+  counts = [0]*len(labels)
+  for label in labels:
+    for val in values:
+      if label == val:
+        counts[labels.index(label)] +=1
+  #Returns mode of the classification
+  return labels[counts.index(max(counts))]
+
+#Tests if the classifications of the examples are homogeneous
+def sameClass(examples):
+  testClass = examples[0]['Class']
+  for example in examples:
+    if example['Class'] != testClass:
+      return False
+  return True
+
+#Decides which attribute is the best to split on by comparing entropy gain
+def findBestAttribute(examples):
+  attributes = examples[0].keys()
+  attributes.remove('Class')
+  bestAttribute = "None"
+  bestGain = 0
+  #Loop through all attributes and check the entropy gained from the split
+  #New entropy will be a weighted average on the entropy
+  for attribute in attributes:
+    totalEntropy = entropy(examples,attribute)
+    values = list(set(list([example[attribute] for example in examples])))
+    counts = len(values)*[0]
+    newEnts = len(values)*[0]
+    for value in values:
+      #splits the examples for the different values of attribute
+      #I am most proud of this line
+      splitExamples = [example for example in examples if example[attribute] == value]
+      counts[values.index(value)] = float(len(splitExamples))
+      newEnts[values.index(value)] = entropy(splitExamples,attribute)
+    total = sum(counts)
+    #Calculated weight average new entropy value
+    newEnt = 0
+    for i in range(0,len(counts)):
+      newEnt += counts[i]*newEnts[i]/total
+    entGained = totalEntropy-newEnt
+    if entGained > bestGain:
+      bestAttribute = attribute
+      bestGain = entGained 
+  return bestAttribute
 
 def prune(node, examples):
   '''
@@ -36,33 +104,28 @@ def entropy(ds, att):
   Values unknown, denoted at "?" are tallied and the entropy calculation 
   removes data points with "?" from the set (e.g. total = 5, two ?'s causes total = 3)
 
-  
   '''
-  #Find the total number of points in the set
-  total = len(ds)
+  #List of all possible characteristics
+  labels = list(set(list([example['Class'] for example in ds])))
+  #Counts of each type of characteristic
+  num = len(labels)
+  counts = [0]*num
+  #Count number of known attributes, and number of each characteristic
+  total = 0;
+  for ex in ds:
+    if ex[att] != '?':
+      total += 1
+      for label in labels:
+        if ex['Class'] == label:
+        counts[labels.index(label)] +=1
 
-  #define and zero variables. 
-  numpos = 0.0
-  numneg = 0.0
-  numunk = 0.0
-
-  #Count number of each republicans and democrats 
-  for i in range (0, total):
-    if ds[i][att] == att: 
-      numpos += 1.0
-    elif ds[i][att] == "?"
-      numunk += 1.0 
-    else:
-      numneg += 1.0
-
-  #Remove unknowns from the set
-  total = total - numunk;
-
+ 
   #Calculate entropy and returns
-  ent = -(numpos/total)*np.log2(numpos/total) - (numneg/total)*np.log2(numneg/total)
+  ent = 0
+  for i in range(0,num):
+    p = float(counts[1])/float(total)
+    ent += - p*math.log(p,num)
   return ent
-
-
 
 def infoGain(ds):
   '''
--- 001_Homework/runner.py
+++ 001_Homework/runner.py
@@ -1,6 +1,5 @@
 from node import Node
 import math
-#import ID3
 from parse import parse
 
 #Christopher Miller
@@ -8,36 +7,117 @@ from parse import parse
 #Winter 2018
 #EECS 349
 
+#Find the most common characteristic of the examples
+def mode(examples):
+	#Get list of all characteristics for the examples
+	values = list([example['Class'] for example in examples])
+	#Get the list of possible characteristics
+	labels = list(set(values))
+	#Find number of instances of each label
+	counts = [0]*len(labels)
+	for label in labels:
+		for val in values:
+			if label == val:
+				counts[labels.index(label)] +=1
+	#Returns mode of the characteristics
+	return labels[counts.index(max(counts))]
+
+def sameClass(examples):
+	testClass = examples[0]['Class']
+	for example in examples:
+		if example['Class'] != testClass:
+			return False
+	return True
+
+#Decides which attribute is the best to split on by comparing entropy gain
+def findBestAttribute(examples):
+	attributes = examples[0].keys()
+	attributes.remove('Class')
+	bestAttribute = "None"
+	bestGain = 0
+	#Loop through all attributes and check the entropy gained from the split
+	#New entropy will be a weighted average on the entropy
+	for attribute in attributes:
+		totalEntropy = entropy(examples,attribute)
+		values = list(set(list([example[attribute] for example in examples])))
+		counts = len(values)*[0]
+		newEnts = len(values)*[0]
+		for value in values:
+			splitExamples = [example for example in examples if example[attribute] == value]
+			counts[values.index(value)] = float(len(splitExamples))
+			newEnts[values.index(value)] = entropy(splitExamples,attribute)
+		total = sum(counts)
+		#Calculated weight average new entropy value
+		newEnt = 0
+		for i in range(0,len(counts)):
+			newEnt += counts[i]*newEnts[i]/total
+		entGained = totalEntropy-newEnt
+		if entGained > bestGain:
+			bestAttribute = attribute
+			bestGain = entGained 
+	print bestGain
+	print bestAttribute
+
+
+def entropy(ds, att):
+  '''
+  Calculates the entropy for an array of examples, ds, in dictionary form with the
+  defining attribute, att. att is a string. 
+
+  Values unknown, denoted at "?" are tallied and the entropy calculation 
+  removes data points with "?" from the set (e.g. total = 5, two ?'s causes total = 3)
+
+  '''
+  #List of all possible characteristics
+  labels = list(set(list([example['Class'] for example in ds])))
+  #Counts of each type of characteristic
+  num = len(labels)
+  counts = [0]*num
+  #Count number of known attributes, and number of each characteristic
+  total = 0;
+  for ex in ds:
+  	if ex[att] != '?':
+  		total += 1
+  		for label in labels:
+  			if ex['Class'] == label:
+				counts[labels.index(label)] +=1
+
+  #Calculate entropy and returns
+  ent = 0
+  for i in range(0,num):
+  	p = float(counts[i])/float(total)
+  	ent += - p*math.log(p,num)
+  return ent
+
+  
+
+
 
 #Read in the data
-data2 = [dict(a=1, b=0, Class=2), 
+data2 = [dict(a=1, b=1, Class=0),
 		dict(a=1, b=1, Class=1),
-	    dict(a=2, b=0, Class=2), 	
-	    dict(a=2, b=1, Class=3),
-    	dict(a=3, b=0, Class=1),
-    	dict(a=3, b=1, Class=3)]
+	    dict(a=1, b=0, Class=1),	
+	    dict(a=1, b=0, Class=1),
+    	dict(a=1, b=0, Class=1),
+    	dict(a=0, b=1, Class=1), 
+		dict(a=0, b=1, Class=0),
+	    dict(a=0, b=1, Class=0), 	
+	    dict(a=0, b=0, Class=0),
+    	dict(a=0, b=0, Class=0)]
 
-data =  [dict(a=1, b=0,   c='?', Class=1), 
+
+data =  [dict(a=1, b=0,   c='?', Class='a'), 
 		 dict(a=1, b=3,   c=2,   Class=1),
 		 dict(a=2, b='?', c=1,   Class=2), 
-		 dict(a=2, b=1,   c=3,   Class=2),
+		 dict(a=2, b=1,   c=3,   Class=3),
          dict(a=3, b=0,   c=1,   Class=3), 
          dict(a=3, b=2,   c='?', Class=3)]
 
+
+
+
+
 #Parse the voting training data
 trainingData = parse('house_votes_84.data')
-#print trainingData[1].keys()[16]
-
-#Num keys -1 (last is the class)
-num_attributes = len(trainingData[1].keys())-1
-
-#Find index in the loop
-classidx = 0
-for i in range(0, num_attributes+1):
-	if(trainingData[1].keys()[i] == "Class"):
-		classidx = i
-		break
-
-#ent = ID3.entropy(data)
-print classidx
+findBestAttribute(data2)
 
